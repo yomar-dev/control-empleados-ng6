4e8c3f6a09e61e3cb1d73057a6a68407bcf51306
Yomar Miranda <mirandayomar@gmail.com>
2018-07-09 07:33:11 -0500

    Rutas Empleados.

--- server/routes/empleados.routes.js
+++ server/routes/empleados.routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json({
-        status: 'Hello world!!'
-    });
-})
+const EmpleadoController = require('../controllers/empleado.controller');
+
+router.get('/', EmpleadoController.obtenerEmpleados);
+router.post('/', EmpleadoController.crearEmpleado);
+router.get('/:id', EmpleadoController.obtenerEmpleado);
+router.put('/:id', EmpleadoController.editarEmpleado);
+router.delete('/:id', EmpleadoController.eliminarEmpleado)
 
 module.exports = router;
